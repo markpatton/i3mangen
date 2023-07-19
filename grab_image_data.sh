59c94dd4218fa5f34ee6ef2c1abd5c84f8ab9068
Mark Patton <mark.patton@gmail.com>
2023-07-19 15:16:59 -0400

    Sort files for csv

--- grab_image_data.sh
+++ grab_image_data.sh
@@ -4,12 +4,12 @@
 
 input_dir="$1"
 image_ext="$2"
-
+pathprefix="$3"
 
 IFS="
 "
-for path in $(find "$input_dir" -name "*.$image_ext" -printf '%P\n')
+for path in $(find "$input_dir" -name "*.$image_ext" -printf '%P\n' | sort)
 do
     wh=$(identify -format "%w,%h" "$input_dir/$path")
-    echo "$path,$wh"
+    echo "${pathprefix}${path},${wh}"
 done
